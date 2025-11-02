039a5717c9799c918f90044794df05c0c3fc08d4
Rohit Mugalya <132080109+RohitMugalya@users.noreply.github.com>
2025-11-02 20:15:17 +0530

    located dino, cactus, ground set coordinates of all sprites, run_speed

--- runner.py
+++ runner.py
@@ -5,8 +5,17 @@ pygame.init()
 
 DIMENSION = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
 SCREEN_BACKGROUND_COLOR = (0, 0, 0)
+
 DINO_DIMENSION = DINO_WIDTH, DINO_HEIGHT = 100, 100
+DINO_COORDINATES = DINO_X, DINO_Y = int(0.1 * SCREEN_WIDTH), int(0.6 * SCREEN_HEIGHT)
+CACTUS_X, CACTUS_Y = int(0.8 * SCREEN_WIDTH), int(0.6 * SCREEN_HEIGHT) + 11
 CACTUS_DIMENSION = CACTUS_WIDTH, CACTUS_HEIGHT = 50, 100
+
+GROUND_COLOR = (0, 139, 139)
+GROUND_THICKNESS = 10
+RUN_SPEED = 5
+
+ground = pygame.Rect(0, DINO_Y + DINO_HEIGHT, SCREEN_WIDTH, GROUND_THICKNESS)
 
 screen = pygame.display.set_mode(DIMENSION)
 pygame.display.set_caption("Chrome Dino AI")
@@ -39,8 +48,10 @@ while running:
         last_switch_time = current_time
 
     screen.fill(SCREEN_BACKGROUND_COLOR)
-    screen.blit(dino_images[dino_index], (400, 200))
-    screen.blit(cactus, (600, 250))
+    pygame.draw.rect(screen, GROUND_COLOR, ground)
+    screen.blit(dino_images[dino_index], DINO_COORDINATES)
+    screen.blit(cactus, (CACTUS_X, CACTUS_Y))
+    CACTUS_X = (CACTUS_X - RUN_SPEED) % SCREEN_WIDTH
 
     pygame.display.flip()
     clock.tick(60)
