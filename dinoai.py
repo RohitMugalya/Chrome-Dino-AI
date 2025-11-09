7486ec49a926038712afc36f7a9abc590c5ac25b
Rohit Mugalya <132080109+RohitMugalya@users.noreply.github.com>
2025-11-09 23:54:11 +0530

    changed self.observations from list to pd.DataFrame

--- dinoai.py
+++ dinoai.py
@@ -6,20 +6,22 @@ class DinoAI:
     def __init__(self):
         self.jump_successful = 1
         self.jump_failed = 0
+        self.features = ["take_off_distance"]
+        self.target = "jump_status"
         self.dataset_path = "observations.csv"
-        self.dataset_headers = ["take_off_distance", "jump_status"]
+        self.dataset_headers = self.features + [self.target]
         self.observations = self.load_observations()
 
     def load_observations(self):
         if os.path.exists(self.dataset_path):
             df = pd.read_csv(self.dataset_path)
-            return df.values.tolist()
+            return df
         else:
-            return []
+            return pd.DataFrame(columns=self.dataset_headers)
 
     def save_observations(self):
-        df = pd.DataFrame(self.observations, columns=self.dataset_headers)
-        df.to_csv(self.dataset_path, index=False)
+        self.observations.to_csv(self.dataset_path, index=False)
 
-    def record_observation(self, take_off_distance, jump_status):
-        self.observations.append([take_off_distance, jump_status])
+    def record_observation(self, take_off_distance: float, jump_status: int):
+        observation = {self.features[0]: take_off_distance, self.target: jump_status}
+        self.observations.loc[len(self.observations)] = observation
