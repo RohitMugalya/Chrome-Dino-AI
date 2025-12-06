59e445892f5891c40f4f0f2725878ad8686f6b2c
Rohit Mugalya <132080109+RohitMugalya@users.noreply.github.com>
2025-12-06 23:24:01 +0530

    implemented load_model with default to start fresh if [], yet to implement loading nth generation

--- dinoai.py
+++ dinoai.py
@@ -1,4 +1,7 @@
 import os
+import joblib
+
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 
 
@@ -12,6 +15,15 @@ class DinoAI:
         self.dataset_headers = self.features + [self.target]
         self.observations = self.load_observations()
 
+    def load_model(self, model_base_dir="generations/"):
+        generations = os.listdir(model_base_dir)
+        generations = [os.path.join(model_base_dir, gen) for gen in generations if os.path.isfile(os.path.join(model_base_dir, gen))]
+        generations.sort()
+        
+        if not generations:
+            return LogisticRegression()
+        
+    
     def load_observations(self):
         if os.path.exists(self.dataset_path):
             df = pd.read_csv(self.dataset_path)
@@ -25,3 +37,8 @@ class DinoAI:
     def record_observation(self, take_off_distance: float, jump_status: int):
         observation = {self.features[0]: take_off_distance, self.target: jump_status}
         self.observations.loc[len(self.observations)] = observation
+
+
+if __name__ == "__main__":
+    dino_ai = DinoAI()
+    dino_ai.load_model()
